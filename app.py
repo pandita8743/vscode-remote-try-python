f8b05a6713917dce9a051c113cc56f04719d1faa
Pandita <146020318+pandita8743@users.noreply.github.com>
2023-11-05 01:02:28 +0000

    Piedra, papel y tijeras en python con Github copilot

--- app.py
+++ app.py
@@ -9,3 +9,27 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
+
+print('hello world')
+
+import random
+
+opciones = ['piedra', 'papel', 'tijeras']
+
+while True:
+    eleccion_usuario = input('Elige una opción (piedra, papel, tijeras): ').lower()
+    if eleccion_usuario not in opciones:
+        print('Opción no válida. Inténtalo de nuevo.')
+        continue
+    eleccion_oponente = random.choice(opciones)
+    if eleccion_usuario == eleccion_oponente:
+        print('Empate!')
+    elif (eleccion_usuario == 'piedra' and eleccion_oponente == 'tijeras') or \
+         (eleccion_usuario == 'tijeras' and eleccion_oponente == 'papel') or \
+         (eleccion_usuario == 'papel' and eleccion_oponente == 'piedra'):
+        print('Ganaste!')
+    else:
+        print('Perdiste!')
+    jugar_de_nuevo = input('¿Quieres jugar de nuevo? (sí/no): ').lower()
+    if jugar_de_nuevo != 'sí':
+        break
